77755565b0ae3ee34182699cdca688640f2795e0
Axel Castells Monllau <axelcastellsmonllau@gmail.com>
2017-11-13 12:10:26 +0100

    Edit

--- js/projectile_prefab.js
+++ js/projectile_prefab.js
@@ -4,28 +4,21 @@ gameEngine.projectile_prefab = function(game,type,x,y,level){
     this.type = type;
     this.game = game;
     
-    this.states;
-    this.currentState;
-    
     Phaser.Sprite.call(this,game,x,y,'enemies');
     
     switch(this.type)
         {
             case SYSTEM_CONSTANTS.PROJECTILE_TYPES.ROCK:
-            {
-                this.states = {INIT: 0, WALK: 1, CHARGE: 2, SHOOT: 3};
-                this.currentState = this.states.INIT;
+            {                
+                this.animations.add('shoot',[34],1,true);
                 
-                this.animations.add('walk_down',[2,3],10,true);
-                this.animations.add('walk_left',[0,1],10,true);
-                this.animations.add('walk_right',[4,5],10,true);
-                this.animations.add('walk_up',[6,7],10,true);
-                
-                this.animations.play('walk_down');
+                this.animations.play('shoot');
             }break;
             case SYSTEM_CONSTANTS.PROJECTILE_TYPES.FIREBALL:
             {
-
+                this.animations.add('shoot',[32,33],10,true);
+                
+                this.animations.play('shoot');
             }break;
             default:
             {
